b1c07c4044f0dbff35e55694a476d7b580fd92ec
anand-clarion <anand.tripathi@clariontechnologies.co.in>
2015-05-19 11:06:28 +0530

    Modified the unit test cases for the post model.

--- Anand/TDD-Practice/spec/models/post_spec.rb
+++ Anand/TDD-Practice/spec/models/post_spec.rb
@@ -3,12 +3,13 @@ require 'spec_helper'
 describe Post do
   let(:post) { FactoryGirl.create(:post) }
 
-  it { should respond_to(:title) }
-  it { should respond_to(:content) }
-  it { should validate_presence_of(:title) }
-  it { should validate_length_of(:title).is_at_least(5) }
-  it { should validate_length_of(:content).is_at_least(10) }
-  it { should validate_length_of(:content).is_at_most(250) }
+  it { is_expected.to respond_to(:title) }
+  it { is_expected.to respond_to(:content) }
+  it { is_expected.to validate_presence_of(:title) }
+  it { is_expected.to validate_presence_of(:content) }
+  it { is_expected.to validate_length_of(:title).is_at_least(5) }
+  it { is_expected.to validate_length_of(:content).is_at_least(10) }
+  it { is_expected.to validate_length_of(:content).is_at_most(250) }
 
   describe "creation" do
     context "valid attributes" do
