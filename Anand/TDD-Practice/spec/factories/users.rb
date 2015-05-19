61f6cb31fdd0f69dadc333c1454d1d3e87147a7e
anand-clarion <anand.tripathi@clariontechnologies.co.in>
2015-05-19 11:03:32 +0530

    Added functional test cases for users  controller

--- Anand/TDD-Practice/spec/factories/users.rb
+++ Anand/TDD-Practice/spec/factories/users.rb
@@ -6,4 +6,11 @@ FactoryGirl.define do
     password "8character"
     password_confirmation "8character"
   end
+
+  factory :invalid_user, class: :user do
+    name ""
+    email "wrong_email"
+    password "8character"
+    password_confirmation "8character"
+  end
 end
